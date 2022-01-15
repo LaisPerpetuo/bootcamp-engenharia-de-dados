ca0cf0327ee514edaaa8d8787164894e867e1d8a
Lais Perpetuo <laisperpetuo9@gmail.com>
2022-01-15 17:32:47 -0300

    [feature] cria datalake [develop-data-lake]

--- app.py
+++ app.py
@@ -5,6 +5,6 @@ from data_platform.common_stack import CommonStack
 
 app = core.App()
 data_lake = DataLakeStack(app)
-common_stack = CommonStack(app)
+#common_stack = CommonStack(app)
 #dms = DmsStack(app, common_stack=common_stack, data_lake_raw_bucket=data_lake.data_lake_raw_bucket)
 app.synth()
